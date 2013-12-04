b7deac7c57fa8c247f77f12c0e3658d8f7931ad7
Todestrieb <sis53lambda@gmail.com>
2013-12-04 00:47:41 -0600

    MySQL

---
+++ mysqlconnect/MySQL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace mysqlconnect
+{
+    
+    class MySQL
+    {
+    protected MySqlConnection MyConnection;
+
+    public MySQL() { }
+
+        protected void abrirConnection(){
+            string connectionString="Server=localhost;" +
+                "Database=Registro;"+
+                "User ID=root;" +
+                "Password=;" +
+                "Pooling= false;";
+            this.MyConnection = new MySqlConnection(connectionString);
+            this.MyConnection.Open();
+            
+        }
+        protected void cerrarConnection(){
+                        this.MyConnection.Close(); 
+                        this.MyConnection = null;
+        
+    }
+    }
+    }
---
+++ mysqlconnect/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace mysqlconnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Registro registros = new Registro();
+            string opcionUsuario = "";
+           
+            do{
+
+
+            Console.WriteLine("Elije una acción qué realizar a la base de datos: \n1-Mostrar información \n2-Editar registro \n3-Eliminar registro \n4-Nuevo registro \n5-Salir");
+           
+            opcionUsuario = Console.ReadLine();
+            if (opcionUsuario == "1")
+            {
+                registros.MostrarRegistros();
+
+            }
+            else if (opcionUsuario == "2")
+            {
+                  registros.EditarCodigoRegistro();
+                  registros.EditarNombreRegistro();
+                  registros.EditarTelefonoRegistro();
+                  registros.EditarEmailRegistro();
+                  Console.WriteLine("Registro editado");
+            }
+           
+            else if (opcionUsuario == "3")
+            {
+                
+                registros.eliminarRegitro();
+                Console.WriteLine("Registro eliminado");
+            }
+            else if (opcionUsuario == "4")
+            {
+                registros.NuevoRegistro();
+                Console.WriteLine("Registro agregado");
+            }
+
+                Console.ReadKey();
+                Console.Clear();
+
+            }while(opcionUsuario != "5");  
+                
+            
+            
+        }
+    }
+}
